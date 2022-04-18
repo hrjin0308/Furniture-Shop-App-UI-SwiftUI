cf0a23a8c47d61e21ae7ac20d282616692cf9bc6
HRJin <wlsgpfla92@gmail.com>
2022-04-18 21:49:16 +0900

    Furniture-Shop-App with youtube to study SwiftUI

--- Furniture_app/ContentView.swift
+++ Furniture_app/ContentView.swift
@@ -8,15 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-        
+  var body: some View {
+    VStack {
+      HomeScreen()
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
